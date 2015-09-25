221ba5de7f161a824027137bb57fa7badf201cda
plusvolt <plusvolt@egiskorea.com>
2015-09-25 15:22:42 +0900

    color까지 구현

--- api2.0.js
+++ api2.0.js
@@ -19,19 +19,19 @@ vw.Object = function(){
 //vw.Object.prototype.userProperties = {};
 
 vw.Object.prototype.on = function(type, listener){
-	//이벤트 타입에 리스너를 등록
+	//이벤트 타입에 리스너를 등록(구현)
 };
 
 vw.Object.prototype.once = function(type, listener){
-	//이벤트 타입에 한번만 동작하는 리스너를 등록
+	//이벤트 타입에 한번만 동작하는 리스너를 등록(구현)
 };
 
 vw.Object.prototype.un = function(type, listener){
-	//이벤트 제거	
+	//이벤트 제거	(구현)
 };
 
 vw.Object.prototype.unByKey = function(key){
-	// 반환된 키로 이벤트 제거
+	// 반환된 키로 이벤트 제거(구현)
 };
 
 /*사용자 프로퍼티 추가*/
@@ -57,8 +57,6 @@ vw.Object.prototype.getNames = function(){
         return null;   
     }
 };
-
-
 
 /*사용자 프로퍼티 제거*/
 vw.Object.prototype.remove = function(name){
@@ -168,6 +166,8 @@ vw.Extent = function(pMinx, pMiny, pMaxx, pMaxy){
     this.maxx = pMaxx;
     this.maxy = pMaxy;
 
+    /*두개의 coord좌표를 이용하여 extent 생성하지
+    @params start : 시작좌표(vw.Coord), end : 끝좌표(vw.Coord)*/
     this.fromConers = function(start, end){
         var rSize = start - end; //좌표로 사이즈 구해야함
         return rSize;
@@ -182,10 +182,253 @@ vw.Extent = function(pMinx, pMiny, pMaxx, pMaxy){
         return copy;       
     }
 }
-
 vw.Extent.prototype = new vw.Size();
 
+/*값이 변할 수 있는 범위를 지정*/
+vw.Bound ={
+  apply : false, //바운드를 지정할지 여부
+  min : 0,
+  max : 999999
+}
 
+/*색상 클래스 
+ *r,g,b,a를 톨해 색상을 생성 및 반환한다
+ *@params r : RED ,g : GREEN,b : BLUE ,a : ALPHA
+ */
+vw.Color = function(pr,pg,pb,pa){
+  if(arguments.length < 3) return false; //생서자 호출시 최소 파라미터 3개
+  
+  for(var i = 0; i < arguments.length; i ++ ){ //RGB범위값 체크
+    if(arguments[i] < 0 && arguments[i] > 255){
+      return false;
+      //this.rgba[Object.keys(this.rgba)[i] = arguments[i];
+    }
+  }
+
+  var aFlag = arguments.length;
+
+  this.r = pr;
+  this.g = pg;
+  this.b = pb;
+
+  if(aFlag == 4 ){ //a 파라미터 있을때만 표시 
+    this.a = pa;  
+  }else{
+    this.a = 1;
+  }  
+}
+
+vw.Color.prototype = new vw.Object(); //vw.Object 상속
+//rgba to color
+vw.Color.prototype.fromRation = function(pr,pg,pb,pa){
+  //구현
+  return true;
+}
+//hex to color
+vw.Color.prototype.fromCssString = function(pColor){
+  //구현
+  return true;
+}
+  //color to hex;
+vw.Color.prototype.getCssString = function(pColor){
+  //구현
+  return true;
+}
+/*사용자 프로퍼티 키 목록 반환*/
+vw.Color.prototype.getNames = function(){
+  var names = new Array();
+  for (var attrC in this) {
+    if(this.hasOwnProperty(attrC)){
+      if(attrC == "userProperties"){//userProperties추가시
+        for(var a in this.userProperties){
+          names.push(a);  
+        }
+      }else{
+        names.push(attrC);  
+      }
+    }
+  }
+  return names;
+};
+
+/*컬러 객체 복제*/
+vw.Object.prototype.clone = function(){
+  if (null == this || "object" != typeof this) return this;
+    var copy = new vw.Color(this.r,this.g,this.b,this.a);
+    for (var attr in this) {
+        if (this.hasOwnProperty(attr)) copy[attr] = this[attr];
+    }
+  return copy;       
+}
+
+vw.ColorRampType = function{};
+
+
+
+
+/*
+vw.Color.names = {
+  'aliceblue': '#f0f8ff',
+  'antiquewhite': '#faebd7',
+  'aqua': '#00ffff',
+  'aquamarine': '#7fffd4',
+  'azure': '#f0ffff',
+  'beige': '#f5f5dc',
+  'bisque': '#ffe4c4',
+  'black': '#000000',
+  'blanchedalmond': '#ffebcd',
+  'blue': '#0000ff',
+  'blueviolet': '#8a2be2',
+  'brown': '#a52a2a',
+  'burlywood': '#deb887',
+  'cadetblue': '#5f9ea0',
+  'chartreuse': '#7fff00',
+  'chocolate': '#d2691e',
+  'coral': '#ff7f50',
+  'cornflowerblue': '#6495ed',
+  'cornsilk': '#fff8dc',
+  'crimson': '#dc143c',
+  'cyan': '#00ffff',
+  'darkblue': '#00008b',
+  'darkcyan': '#008b8b',
+  'darkgoldenrod': '#b8860b',
+  'darkgray': '#a9a9a9',
+  'darkgreen': '#006400',
+  'darkgrey': '#a9a9a9',
+  'darkkhaki': '#bdb76b',
+  'darkmagenta': '#8b008b',
+  'darkolivegreen': '#556b2f',
+  'darkorange': '#ff8c00',
+  'darkorchid': '#9932cc',
+  'darkred': '#8b0000',
+  'darksalmon': '#e9967a',
+  'darkseagreen': '#8fbc8f',
+  'darkslateblue': '#483d8b',
+  'darkslategray': '#2f4f4f',
+  'darkslategrey': '#2f4f4f',
+  'darkturquoise': '#00ced1',
+  'darkviolet': '#9400d3',
+  'deeppink': '#ff1493',
+  'deepskyblue': '#00bfff',
+  'dimgray': '#696969',
+  'dimgrey': '#696969',
+  'dodgerblue': '#1e90ff',
+  'firebrick': '#b22222',
+  'floralwhite': '#fffaf0',
+  'forestgreen': '#228b22',
+  'fuchsia': '#ff00ff',
+  'gainsboro': '#dcdcdc',
+  'ghostwhite': '#f8f8ff',
+  'gold': '#ffd700',
+  'goldenrod': '#daa520',
+  'gray': '#808080',
+  'green': '#008000',
+  'greenyellow': '#adff2f',
+  'grey': '#808080',
+  'honeydew': '#f0fff0',
+  'hotpink': '#ff69b4',
+  'indianred': '#cd5c5c',
+  'indigo': '#4b0082',
+  'ivory': '#fffff0',
+  'khaki': '#f0e68c',
+  'lavender': '#e6e6fa',
+  'lavenderblush': '#fff0f5',
+  'lawngreen': '#7cfc00',
+  'lemonchiffon': '#fffacd',
+  'lightblue': '#add8e6',
+  'lightcoral': '#f08080',
+  'lightcyan': '#e0ffff',
+  'lightgoldenrodyellow': '#fafad2',
+  'lightgray': '#d3d3d3',
+  'lightgreen': '#90ee90',
+  'lightgrey': '#d3d3d3',
+  'lightpink': '#ffb6c1',
+  'lightsalmon': '#ffa07a',
+  'lightseagreen': '#20b2aa',
+  'lightskyblue': '#87cefa',
+  'lightslategray': '#778899',
+  'lightslategrey': '#778899',
+  'lightsteelblue': '#b0c4de',
+  'lightyellow': '#ffffe0',
+  'lime': '#00ff00',
+  'limegreen': '#32cd32',
+  'linen': '#faf0e6',
+  'magenta': '#ff00ff',
+  'maroon': '#800000',
+  'mediumaquamarine': '#66cdaa',
+  'mediumblue': '#0000cd',
+  'mediumorchid': '#ba55d3',
+  'mediumpurple': '#9370db',
+  'mediumseagreen': '#3cb371',
+  'mediumslateblue': '#7b68ee',
+  'mediumspringgreen': '#00fa9a',
+  'mediumturquoise': '#48d1cc',
+  'mediumvioletred': '#c71585',
+  'midnightblue': '#191970',
+  'mintcream': '#f5fffa',
+  'mistyrose': '#ffe4e1',
+  'moccasin': '#ffe4b5',
+  'navajowhite': '#ffdead',
+  'navy': '#000080',
+  'oldlace': '#fdf5e6',
+  'olive': '#808000',
+  'olivedrab': '#6b8e23',
+  'orange': '#ffa500',
+  'orangered': '#ff4500',
+  'orchid': '#da70d6',
+  'palegoldenrod': '#eee8aa',
+  'palegreen': '#98fb98',
+  'paleturquoise': '#afeeee',
+  'palevioletred': '#db7093',
+  'papayawhip': '#ffefd5',
+  'peachpuff': '#ffdab9',
+  'peru': '#cd853f',
+  'pink': '#ffc0cb',
+  'plum': '#dda0dd',
+  'powderblue': '#b0e0e6',
+  'purple': '#800080',
+  'red': '#ff0000',
+  'rosybrown': '#bc8f8f',
+  'royalblue': '#4169e1',
+  'saddlebrown': '#8b4513',
+  'salmon': '#fa8072',
+  'sandybrown': '#f4a460',
+  'seagreen': '#2e8b57',
+  'seashell': '#fff5ee',
+  'sienna': '#a0522d',
+  'silver': '#c0c0c0',
+  'skyblue': '#87ceeb',
+  'slateblue': '#6a5acd',
+  'slategray': '#708090',
+  'slategrey': '#708090',
+  'snow': '#fffafa',
+  'springgreen': '#00ff7f',
+  'steelblue': '#4682b4',
+  'tan': '#d2b48c',
+  'teal': '#008080',
+  'thistle': '#d8bfd8',
+  'tomato': '#ff6347',
+  'turquoise': '#40e0d0',
+  'violet': '#ee82ee',
+  'wheat': '#f5deb3',
+  'white': '#ffffff',
+  'whitesmoke': '#f5f5f5',
+  'yellow': '#ffff00',
+  'yellowgreen': '#9acd32'
+};
+
+*/
+
+
+
+
+
+
+
+
+
+
+/*공통 유틸 관련 클래스*/
 vw.Util = {};
 vw.Util.unSafeClone = function(obj){
    var type = vw.typeOf(obj);
@@ -203,16 +446,6 @@ vw.Util.unSafeClone = function(obj){
   return obj;
 }
 
-
-function flatClone(obj){
-    if (null == obj || "object" != typeof obj) return obj;
-    var cloneObj = new obj.constructor();
-    for (var attr in obj) {
-        console.log(attr);
-        if (this.hasOwnProperty(cloneObj)) obj[attr] = cloneObj[attr];
-    }
-    return cloneObj;
-}
 
 
 
